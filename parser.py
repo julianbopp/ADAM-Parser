6e94d6b2281c8994bdd0b326e406297cc6ebd51c
julianbopp <julian.bopp@tutanota.com>
2022-09-19 13:43:38 +0200

    add static post data that will be used in post requests

--- parser.py
+++ parser.py
@@ -22,3 +22,31 @@ postHeaders = {
     "Sec-Fetch-Mode": "navigate",
     "Sec-Fetch-Site": "cross-site"
 }
+
+firstPostData = {
+    "user_idp": "https://aai-logon.unibas.ch/idp/shibboleth",
+    "Select": "Select"
+}
+
+secondPostData = {
+    "shib_idp_ls_exception.shib_idp_session_ss": "",
+    "shib_idp_ls_success.shib_idp_session_ss": "true",
+    "shib_idp_ls_value.shib_idp_session_ss": "",
+    "shib_idp_ls_exception.shib_idp_persistent_ss": "",
+    "shib_idp_ls_success.shib_idp_persistent_ss": "true",
+    "shib_idp_ls_value.shib_idp_persistent_ss": "",
+    "shib_idp_ls_supported": "true",
+    "_eventId_proceed": ""
+}
+
+thirdPostData = {
+    "j_username": username,
+    "j_password": password,
+    "_eventId_proceed": ""
+}
+
+fourthPostData = {
+    "shib_idp_ls_exception.shib_idp_session_ss": "",
+    "shib_idp_ls_success.shib_idp_session_ss": "false",
+    "_eventId_proceed": ""
+}
