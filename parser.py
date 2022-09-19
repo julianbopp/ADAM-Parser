4ff981780c4d3f5923e834bf28604feba2171122
julianbopp <julian.bopp@tutanota.com>
2022-09-19 13:42:50 +0200

    add header data

--- parser.py
+++ parser.py
@@ -2,3 +2,23 @@ import requests
 
 username = input("insert username: ")
 password = input("insert password: ")
+
+headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0",
+           "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+           "Accept-Language": "en-US,en;q=0.5",
+           "Content-Type": "application/x-www-form-urlencoded",
+           "Upgrade-Insecure-Requests": "1",
+           "Sec-Fetch-Dest": "document",
+           "Sec-Fetch-Mode": "navigate",
+           "Sec-Fetch-Site": "same-origin"}
+
+postHeaders = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "cross-site"
+}
